e3340b22ddd04d5ec4aaf84f6d72f788ed6b1c69
dustyraven <dusty@gbg.bg>
2016-02-14 17:10:20 +0000

    .

--- gpio_down.sh
+++ gpio_down.sh
@@ -3,17 +3,17 @@
 #sudo ln -s /home/pi/terrasens/gpio_down.sh /etc/init.d/gpio_down.sh
 #sudo update-rc.d gpio_down.sh defaults
 
-gpio -g mode 2 out
-gpio -g write 2 0
-gpio -g mode 3 out
-gpio -g write 3 0
-gpio -g mode 4 out
-gpio -g write 4 0
-gpio -g mode 17 out
-gpio -g write 17 0
-gpio -g mode 27 out
-gpio -g write 27 0
-gpio -g mode 22 out
-gpio -g write 22 0
+/usr/bin/gpio -g mode 2 out
+/usr/bin/gpio -g write 2 0
+/usr/bin/gpio -g mode 3 out
+/usr/bin/gpio -g write 3 0
+/usr/bin/gpio -g mode 4 out
+/usr/bin/gpio -g write 4 0
+/usr/bin/gpio -g mode 17 out
+/usr/bin/gpio -g write 17 0
+/usr/bin/gpio -g mode 27 out
+/usr/bin/gpio -g write 27 0
+/usr/bin/gpio -g mode 22 out
+/usr/bin/gpio -g write 22 0
 
 
